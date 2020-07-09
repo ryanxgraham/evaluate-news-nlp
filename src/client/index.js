df7840bb9882c879702ea1cb8568245c1b27ea93
ryanxgraham <ryanxgraham@gmail.com>
2020-07-09 09:49:49 -0500

    fixed missing import in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
 import { validURL } from './js/urlChecker'
-import { handleSubmit } from './js/formHandler'
+import { handleSubmit, postData } from './js/formHandler'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
